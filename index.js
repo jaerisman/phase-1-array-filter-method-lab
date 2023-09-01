2e62338a3a13721440b7d2a6ae060cf41ac2774b
Jacob Erisman <jacob.erisman@gmail.com>
2023-08-31 21:41:57 -0400

    Lab Completed - JE

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solution here
+const drivers = [
+    {
+        firstName: 'Maisie',
+        homeTown: 'Highlands Ranch'
+    },
+    {
+        firstName: 'Jacob',
+        homeTown: 'Denver'
+    },
+    {
+        firstName: 'Sarah',
+        homeTown: 'Aiken'
+    }, 
+    {
+        firstName: 'Oliver',
+        homeTown: 'Aiken'
+    },
+]
+
+function findMatching(collection, name){
+    return collection.filter(function(driver) {
+     return driver.toLowerCase() === name.toLowerCase();
+    });
+ };
+
+function fuzzyMatch(collection, query){
+    return collection.filter(function(driver) {
+        return driver.startsWith(query);
+    });
+ }
+
+ function matchName(collection, name){
+    return collection.filter(function(driver) {
+        return driver.name === name;
+    });
+ }
